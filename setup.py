d7d6db548adb6154815e4b0523f108596fef2be2
Anoint <72187543+whoami-anoint@users.noreply.github.com>
2023-09-06 17:06:43 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PreProData',
+    version='0.2',
+    packages=find_packages(),
+    install_requires=[
+        'scikit-learn',  # You can add more dependencies here
+    ],
+)
